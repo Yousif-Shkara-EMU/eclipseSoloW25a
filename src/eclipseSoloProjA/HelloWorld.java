bdd2bbd31b0e001eb4d590c5031335f5708fb63d
Yousif-Shkara <yousifshkara9@gmail.com>
2025-02-03 12:21:11 -0500

    Commit 2

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,7 +3,7 @@ package eclipseSoloProjA;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello github project"); //
+		System.out.println("Hello github project"); 
 
 	}
 
