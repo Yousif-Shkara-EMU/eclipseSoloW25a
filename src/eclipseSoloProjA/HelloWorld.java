9486218b10c9296e8b3e5706360c50982cd02ece
Yousif-Shkara <yousifshkara9@gmail.com>
2025-02-10 04:07:43 -0500

    Reverted back to get screenshot

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,10 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello github project"); 
 		System.out.println("Another println!");
-
+<<<<<<< HEAD
 		System.out.println("ProgrammerOne did this");
 
-
+=======
+		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 1 did this");
+>>>>>>> branch 'master' of https://github.com/Yousif-Shkara-EMU/eclipseSoloW25a
 	}
 
 }
