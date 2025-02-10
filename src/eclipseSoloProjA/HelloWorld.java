d6a8c01ceaf4736810c287492e402a3b523cabf8
Yousif-Shkara <yousifshkara9@gmail.com>
2025-02-10 04:08:07 -0500

    Step 11

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,13 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello github project"); 
 		System.out.println("Another println!");
-<<<<<<< HEAD
+
 		System.out.println("ProgrammerOne did this");
 
-=======
-		System.out.println("Programmer 2 did this");
-		System.out.println("Programmer 1 did this");
->>>>>>> branch 'master' of https://github.com/Yousif-Shkara-EMU/eclipseSoloW25a
 	}
 
 }
