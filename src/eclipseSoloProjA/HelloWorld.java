63e275669ddbde19f2b730e25208b322b5898624
Yousif-Shkara <yousifshkara9@gmail.com>
2025-02-10 04:01:19 -0500

    Step 8.2 and 8.3 Changed println

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello github project"); 
 		System.out.println("Another println!");
-		System.out.println("Programmer 2 did this");
+		System.out.println("ProgrammerOne did this");
 
 	}
 
