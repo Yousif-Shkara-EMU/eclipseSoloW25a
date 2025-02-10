ae090b9c017d40ee0f0fee9cb735b6cd5df61db3
Yousif-Shkara <yousifshkara9@gmail.com>
2025-02-10 03:53:06 -0500

    Added println

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello github project"); 
 		System.out.println("Another println!");
 		System.out.println("Programmer 2 did this");
-
+		System.out.println("Programmer 1 did this");
 	}
 
 }
